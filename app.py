ed21aada9621f8be6acb8eeab6837370637400bc
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-04-03 19:41:49 -0300

    add deposit request

--- app.py
+++ app.py
@@ -19,18 +19,14 @@ class Account(Resource):
       return {'message': 'Account not found'}, 404
     return {'balance': accounts[account_id]['balance']}
   
-  def post(self, account_id):
-    parser = reqparse.RequestParser()
-    parser.add_argument('deposit', type=float, required=True, help='Deposit amount must be a valid number')
-    args = parser.parse_args();
-
+  def post(self, account_id, deposit=None):
     if account_id not in accounts:
       return {'error': 'Account not found'}, 404  
     
-    accounts[account_id]['balance'] += args['deposit']
+    accounts[account_id]['balance'] += deposit
     return {'balance': accounts[account_id]['balance']}
   
-api.add_resource(Account, '/<string:account_id>')
+api.add_resource(Account, '/<string:account_id>', '/<string:account_id>/<float:deposit>')
 
 if __name__ == '__main__':
     app.run(debug=True)
